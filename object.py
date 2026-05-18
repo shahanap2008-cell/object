22ea48b7ebd8a291600272338a6ffe0d2bd3b915
shahana <shahanap2008@gmail.com>
2026-05-18 14:20:29 +0530

    added name.py

--- object.py
+++ object.py
@@ -15,11 +15,14 @@
 
 
 class person:
-    def __init__(self , name , age):
-        self.name = name
-        self.age = age
+  def __init__(self , name , age):
+      self.name = name
+      self.age = age
 
 p1 = person("emil",36)
 
 print(p1.name)
 print(p1.age)
+
+
+
